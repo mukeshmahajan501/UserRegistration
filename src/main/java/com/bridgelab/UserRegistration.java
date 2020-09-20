3e18ada69bac7e31af303c6e7bc73ba8b9f12e72
mukesh <mukeshmahajan501@gmail.com>
2020-09-20 21:31:02 +0530

    [Mukesh]Added:test cases for mobile number

--- src/main/java/com/bridgelab/UserRegistration.java
+++ src/main/java/com/bridgelab/UserRegistration.java
@@ -1,8 +1,8 @@
 package com.bridgelab;
 
 public class UserRegistration {
-    //Method for checking email
-    public boolean email (String email) {
-        return email.matches("^[a-zA-Z0-9]+(([._+-]?)[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2})*$");
+    //Method for checking mobile number
+    public boolean mobileNumber (String mobileNumber){
+        return mobileNumber.matches("^[0-9]{2}[ ][1-9]{1}[0-9]{9}");
     }
 }
--- src/test/java/com/bridgelab/UserRegistrationTest.java
+++ src/test/java/com/bridgelab/UserRegistrationTest.java
@@ -6,22 +6,31 @@ import org.junit.Test;
 public class UserRegistrationTest {
     UserRegistration userRegistration = new UserRegistration();
     boolean result;
-    //Test cases for email
+
+    //Test cases for mobile number.
     @Test
-    public void email_returnTrue_ifEmailIsProper(){
-        result = userRegistration.email("mukesh.mahajan@gmail.co.in");
+    public void mobileNumber_returnTrue_ifMobileNumberIsProper() {
+        result = userRegistration.mobileNumber("91 7008409012");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void email_returnFalse_ifEmailIsNotProper(){
-        result = userRegistration.email("mukesh.mahajan$blz.co.in");
+    public void mobileNumber_returnFalse_ifMobileNumberIsNotTenDigits() {
+        result = userRegistration.mobileNumber("91 700840901");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void email_returnFalse_ifInEmailSingleDotIsNotThere(){
-        result = userRegistration.email("mukesh.mahajan@gmail..com");
+    public void mobileNumber_returnFalse_ifNoSpaceAfterCountryCode() {
+        result = userRegistration.mobileNumber("917008409012");
         Assert.assertFalse(result);
+
+    }
+
+    @Test
+    public void mobileNumber_retunFalse_ifNumberIsStartWithzero() {
+        result = userRegistration.mobileNumber("91 0308409012");
+        Assert.assertFalse(result);
+
     }
 }
