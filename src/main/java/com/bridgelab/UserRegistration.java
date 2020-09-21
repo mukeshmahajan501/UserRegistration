a235018b9e6778751608ead0f762dc4beaf81ba0
mukesh <mukeshmahajan501@gmail.com>
2020-09-21 08:41:49 +0530

    [Mukesh]Added:test cases for all emails

--- src/main/java/com/bridgelab/UserRegistration.java
+++ src/main/java/com/bridgelab/UserRegistration.java
@@ -1,8 +1,15 @@
 package com.bridgelab;
 
 public class UserRegistration {
-    //Method for checking valid password.
-    public boolean password (String password) {
-        return password.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[*.!@#$%^&(){}:'<>,.?/~`_+-=|].).{8,}$");
+    //Method for checking email.
+    public boolean validateEmail (String email) {
+        String patternEmail = "^[a-zA-Z0-9]+(([._+-]?)[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4}$)*";
+        if (email.matches(patternEmail)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
+
 }
--- src/test/java/com/bridgelab/UserRegistrationTest.java
+++ src/test/java/com/bridgelab/UserRegistrationTest.java
@@ -2,21 +2,56 @@ package com.bridgelab;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+@RunWith(Parameterized.class)
 public class UserRegistrationTest {
     UserRegistration userRegistration = new UserRegistration();
     boolean result;
+    private final String keys;
+    private final boolean values;
 
-    //Test cases for password rule 4 - exactly one special character.
-    @Test
-    public void password_returnTrue_ifPasswordHavingAleastOneSpecialCharacter(){
-        result = userRegistration.password("Abce1defg1@");
-        Assert.assertTrue(result);
+    public UserRegistrationTest(String keys, boolean values) {
+        this.keys = keys;
+        this.values = values;
+    }
+
+    //Test cases for email
+    @Parameterized.Parameters
+    public static Collection<Object[]> testCasesForEmail() {
+        Object[][] testingEmails = new Object[][]{
+                {"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc@1.com", true},
+                {"abc.100@abc.com.au",true},
+                {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true},
+                {"abc",false},
+                {"abc@.com.my",false},
+                {"abc123@gmail.a",false},
+                {"abc123@.com",false},
+                {"abc123@.com.com",false},
+                {".abc@abc.com",false},
+                {".abc()*@gmail.com",false},
+                {"abc@%*.com",false},
+                {"abc..2002@gmail.com",false},
+                {"abc.@gmail.com",false},
+                {"abc@abc@gmail.com",false},
+               {"abc@gmail.com.1a",false},
+                {"abc@gmail.com.aa.au",false},
+        };
+        return Arrays.asList(testingEmails);
     }
 
     @Test
-    public void password_returnFalse_ifPasswordNotHavingAleastOneSecialCharacter(){
-        result = userRegistration.password("Abcedefghi");
-        Assert.assertFalse(result);
+    public void validEmail() {
+        boolean result = userRegistration.validateEmail(keys);
+        Assert.assertEquals(result, values);
     }
 }
